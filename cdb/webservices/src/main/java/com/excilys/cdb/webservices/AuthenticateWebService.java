e46b9cfc9b060397f5557c61233588c3c96218bc
Vogel <jvogel@excilys.com>
2018-06-20 16:50:34 +0200

    [PROTOTYPE] en cours inscription

--- cdb/webservices/src/main/java/com/excilys/cdb/webservices/AuthenticateWebService.java
+++ cdb/webservices/src/main/java/com/excilys/cdb/webservices/AuthenticateWebService.java
@@ -17,11 +17,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.excilys.cdb.webservices.security.JwtAuthenticationRequest;
 import com.excilys.cdb.webservices.security.JwtAuthenticationResponse;
 import com.excilys.cdb.webservices.security.JwtTokenUtil;
 import com.excilys.cdb.webservices.security.JwtUser;
+import com.excilys.cdb.webservices.security.UserSecurityService;
 
 @RestController
 @RequestMapping("/authenticate")
@@ -40,27 +42,36 @@ public class AuthenticateWebService {
 	private UserDetailsService userDetailsService;
 
 	@PostMapping(value = "${jwt.route.authentication.path}")
-	public ResponseEntity<JwtAuthenticationResponse> createAuthenticationToken(@RequestBody JwtAuthenticationRequest authenticationRequest)
-			throws AuthenticationException {
+	public ResponseEntity<JwtAuthenticationResponse> createAuthenticationToken(
+			@RequestBody JwtAuthenticationRequest authenticationRequest) throws AuthenticationException {
 		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
 		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
 		final String token = jwtTokenUtil.generateToken(userDetails);
 		return ResponseEntity.ok(new JwtAuthenticationResponse(token));
 	}
-	
-    @GetMapping(value = "${jwt.route.authentication.refresh}")
-    public ResponseEntity<?> refreshAndGetAuthenticationToken(HttpServletRequest request) {
-        String authToken = request.getHeader(tokenHeader);
-        final String token = authToken.substring(7);
-        String username = jwtTokenUtil.getUsernameFromToken(token);
-        JwtUser user = (JwtUser) userDetailsService.loadUserByUsername(username);
-        if (jwtTokenUtil.canTokenBeRefreshed(token, user.getLastPasswordResetDate())) {
-            String refreshedToken = jwtTokenUtil.refreshToken(token);
-            return ResponseEntity.ok(new JwtAuthenticationResponse(refreshedToken));
-        } else {
-            return ResponseEntity.badRequest().body(null);
-        }
-    }
+
+	@GetMapping(value = "${jwt.route.authentication.refresh}")
+	public ResponseEntity<?> refreshAndGetAuthenticationToken(HttpServletRequest request) {
+		String authToken = request.getHeader(tokenHeader);
+		final String token = authToken.substring(7);
+		String username = jwtTokenUtil.getUsernameFromToken(token);
+		JwtUser user = (JwtUser) userDetailsService.loadUserByUsername(username);
+		if (jwtTokenUtil.canTokenBeRefreshed(token, user.getLastPasswordResetDate())) {
+			String refreshedToken = jwtTokenUtil.refreshToken(token);
+			return ResponseEntity.ok(new JwtAuthenticationResponse(refreshedToken));
+		} else {
+			return ResponseEntity.badRequest().body(null);
+		}
+	}
+
+	/**
+	 * Direction accueil.
+	 * @return nom de la jsp
+	 */
+	@PostMapping("/signup")
+	public ModelAndView signin(@RequestBody JwtAuthenticationRequest auth) {
+		((UserSecurityService) userDetailsService).inscription(auth.getPassword(), encoded);
+	}
 
 	/**
 	 * Authenticates the user. If something is wrong, an
