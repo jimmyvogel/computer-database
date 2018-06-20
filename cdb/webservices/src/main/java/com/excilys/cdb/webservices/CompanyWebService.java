e37bb6cc018437188d98cadb031922d92bdf52c3
jmdebicki <jmdebicki@excilys.com>
2018-06-20 10:06:44 +0200

    [FIX] CrossOrigin on controllers

--- cdb/webservices/src/main/java/com/excilys/cdb/webservices/CompanyWebService.java
+++ cdb/webservices/src/main/java/com/excilys/cdb/webservices/CompanyWebService.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,7 @@ import com.excilys.cdb.webservices.ressources.UrlParams;
 
 @RestController
 @RequestMapping("/company")
+@CrossOrigin
 public class CompanyWebService {
 
 	private ICompanyService serviceCompany;
--- cdb/webservices/src/main/java/com/excilys/cdb/webservices/ComputerWebService.java
+++ cdb/webservices/src/main/java/com/excilys/cdb/webservices/ComputerWebService.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,7 @@ import com.excilys.cdb.webservices.ressources.UrlParams;
 
 @Controller
 @RequestMapping("/computer")
+@CrossOrigin
 public class ComputerWebService {
 
 	private IComputerService serviceComputer;
