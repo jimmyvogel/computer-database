7f564673fbfe3f12f6bef1f61e44e01abb5e38b5
Vogel <jvogel@excilys.com>
2018-06-20 17:04:12 +0200

    [FEAT] inscription in webservice

--- cdb/webservices/src/main/java/com/excilys/cdb/webservices/AuthenticateWebService.java
+++ cdb/webservices/src/main/java/com/excilys/cdb/webservices/AuthenticateWebService.java
@@ -6,23 +6,26 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
+import com.excilys.cdb.model.User;
 import com.excilys.cdb.webservices.security.JwtAuthenticationRequest;
 import com.excilys.cdb.webservices.security.JwtAuthenticationResponse;
 import com.excilys.cdb.webservices.security.JwtTokenUtil;
 import com.excilys.cdb.webservices.security.JwtUser;
+import com.excilys.cdb.webservices.security.JwtUserFactory;
 import com.excilys.cdb.webservices.security.UserSecurityService;
 
 @RestController
@@ -69,8 +72,10 @@ public class AuthenticateWebService {
 	 * @return nom de la jsp
 	 */
 	@PostMapping("/signup")
-	public ModelAndView signin(@RequestBody JwtAuthenticationRequest auth) {
-		((UserSecurityService) userDetailsService).inscription(auth.getPassword(), encoded);
+	public ResponseEntity<JwtUser> signin(@RequestBody JwtAuthenticationRequest auth) {
+		User user = ((UserSecurityService) userDetailsService).inscription(auth.getPassword(), new BCryptPasswordEncoder().encode(auth.getPassword()));
+		JwtUser userJwt = JwtUserFactory.create(user);
+		return new ResponseEntity<JwtUser>(userJwt, HttpStatus.CREATED);
 	}
 
 	/**
