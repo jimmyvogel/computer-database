c34f92687aadc9c95ec4e84facbc5f0aac37bc53
Vogel <jvogel@excilys.com>
2018-06-21 10:57:00 +0200

    [RESET] state

---
+++ cdb/core/src/main/java/com/excilys/cdb/model/Authorities.java
@@ -0,0 +1,45 @@
+package com.excilys.cdb.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Authorities")
+public class Authorities {
+	
+	public enum Role {
+		ROLE_USER,
+		ROLE_ADMIN;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "authority")
+	private String authority;
+
+
+	/**
+	 * Constructor par défaut.
+	 */
+	public Authorities() {
+	}
+	
+	public Authorities(Role roleUser) {
+		this.authority = roleUser.toString();
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+}
